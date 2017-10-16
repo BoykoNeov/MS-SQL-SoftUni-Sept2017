a8feb6db97aa854d64d1862f6db00c7888931929
BoykoNeov <boikoneov@gmail.com>
2017-10-16 08:58:11 +0300

    :apple: grinding...

--- 08-Functions_and_procedures.sql
+++ 08-Functions_and_procedures.sql
@@ -255,20 +255,24 @@ GO
 USE DIABLO
 GO
 
-CREATE OR ALTER FUNCTION ufn_CashInUsersGames (@GameName VARCHAR(50))
-RETURNS MONEY
-BEGIN
-DECLARE @Result MONEY
-RETURN 5
-END
+ CREATE OR ALTER FUNCTION ufn_CashInUsersGames (@gameName nvarchar(50))
+RETURNS TABLE
+AS
 
+DECLARE @Result TABLE
+RETURN (
+  WITH CTE_CashInRows (Cash, RowNumber) AS (
+    SELECT ug.Cash, ROW_NUMBER() OVER (ORDER BY ug.Cash DESC)
+    FROM UsersGames AS ug
+    JOIN Games AS g ON ug.GameId = g.Id
+    WHERE g.Name = @gameName
+  )
+  SELECT SUM(Cash) AS SumCash
+  FROM CTE_CashInRows
+  WHERE RowNumber % 2 = 1 -- odd row numbers only
+  )
+
+-- testing
 GO
-
-DECLARE @SearchedGameID VARCHAR(50)
-SET @SearchedGameID = (SELECT Games.Id FROM GAMES
-WHERE Games.Name = 'Love in a mist')
-
-SELECT * FROM UsersGames
-WHERE GameId = @SearchedGameID
-
--- SELECT * FROM UsersGames WHERE GameID = 48
+SELECT * FROM dbo.ufn_CashInUsersGames ('Lily Stargazer');
+SELECT * FROM dbo.ufn_CashInUsersGames ('Love in a mist');
