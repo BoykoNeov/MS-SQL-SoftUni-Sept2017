fc2cf2affce888c4f66094d4cfb369f5a6aa28b2
BoykoNeov <boikoneov@gmail.com>
2017-10-19 18:59:05 +0300

    :chestnut: finished exam from 19 feb 2017

--- 10AdditionalExcercises.sql
+++ 10AdditionalExcercises.sql
@@ -6,3 +6,42 @@ FROM Users
 ) AS subquery
 GROUP BY Domaines
 ORDER BY NumberOfUsers DESC, Domaines
+
+-- TASK 2
+SELECT 
+Games.Name AS 'Game',
+ GameTypes.Name AS GameType,
+  Users.Username,
+   UsersGames.Level,
+    UsersGames.Cash,
+	 Characters.Name AS Character
+FROM UsersGames
+JOIN Users ON UsersGames.UserId = Users.Id
+JOIN Games ON Games.Id = UsersGames.GameId
+JOIN GameTypes ON Games.GameTypeId = GameTypes.Id
+JOIN Characters ON Characters.Id = UsersGames.CharacterId
+ORDER BY UsersGames.Level DESC, Users.Username ASC, Games.Name ASC
+
+-- TASK 3
+GO
+
+SELECT
+subquery1.Username,
+subquery1.Game,
+COUNT(*) AS ItemsCount,
+ SUM(subquery1.ItemsPrice) AS ItemsPrice
+  
+FROM
+(
+SELECT Users.Username, Games.Name AS Game, Items.Id AS ItemsID, Items.Price AS ItemsPrice
+FROM Users
+JOIN UsersGames ON Users.Id = UsersGames.UserId
+JOIN Games ON Games.Id = UsersGames.GameId
+JOIN UserGameItems ON UserGameItems.UserGameId = UsersGames.Id
+JOIN Items ON Items.Id = UserGameItems.ItemId
+) AS subquery1
+GROUP BY subquery1.Game, subquery1.Username
+HAVING COUNT(*) >= 10
+ORDER BY ItemsCount DESC, ItemsPrice DESC, subquery1.Username ASC
+
+-- TASK 4
